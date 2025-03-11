fcfa4ea0cf07cbfdf87dcb15afc080392a259910
Сутягин Илья <ilya.sutiagin2012@yandex.ru>
2025-03-11 16:31:50 +0400

    Update styles in UpdateButton component

--- src/components/UpdateButton.tsx
+++ src/components/UpdateButton.tsx
@@ -5,7 +5,7 @@ export const UpdateButton = () => {
     return (
         <div>
             {/*тут вывести ошибку*/}
-            <button className='flex w-[204px] h-[56px] bg-[#EB0237] rounded-sm gap-[10px] px-[40px] py-[17px]'>
+            <button className='flex w-[204px] h-[56px] transition-colors duration-300 ease-in-out bg-[#EB0237] hover:bg-[#A01131] active:bg-[#701328] rounded-sm gap-[10px] px-[40px] py-[17px]'>
                 <span className='w-[88px] h-[22px] font-semibold text-lg'>Обновить</span>
                 <Image src='/Refresh.svg' alt='loader' width={26} height={26} />
             </button>
