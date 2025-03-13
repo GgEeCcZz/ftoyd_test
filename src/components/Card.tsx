1eba3751ea65eab4c21c7c7c9d29c5040bdcda13
Сутягин Илья <ilya.sutiagin2012@yandex.ru>
2025-03-13 15:31:50 +0400

    Optimised Image tags

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,7 +12,7 @@ export const Card = ({homeTeam, awayTeam, children}: CardProps) => {
         <div className="w-full h-21.75 rounded-sm px-9 py-4 bg-gradient-to-r from-custom-light-gradient to-custom-dark-gradient">
             <div className="flex justify-between items-center w-full h-13.75">
                 <div className="flex items-center w-43.25 h-12 gap-3.5">
-                    <Image src="/team_logo.svg" alt="teamLogo" width={48} height={48}/>
+                    <Image src="/team_logo.svg" alt="teamLogo" width={48} height={48} className="w-12 h-12"/>
                     <span
                         className="w-27.75 h-4.75 font-semibold text-base leading-[100%] tracking-[0%]">{homeTeam}</span>
                 </div>
@@ -20,7 +20,7 @@ export const Card = ({homeTeam, awayTeam, children}: CardProps) => {
                 <div className="flex items-center w-43.25 h-12 gap-3.5">
                     <span
                         className="w-27.75 h-4.75 font-semibold text-base leading-[100%] tracking-[0%]">{awayTeam}</span>
-                    <Image src="/team_logo.svg" alt="teamLogo" width={48} height={48}/>
+                    <Image src="/team_logo.svg" alt="teamLogo" width={48} height={48} className="w-12 h-12"/>
                 </div>
             </div>
         </div>
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -17,8 +17,8 @@ export const Content = ({ initialMatches, initialError }: ContentProps) => {
 
     return (
         <div>
-            <div className="flex justify-between mb-5">
-                <Image src="/MatchTracker_logo.svg" alt="logo" width={257} height={32} />
+            <div className="flex items-center justify-between mb-5">
+                <Image src="/MatchTracker_logo.svg" alt="logo" width={257} height={32} priority={true} className="w-64.25 h-8"/>
                 <div className="flex justify-end w-295.5 h-14 gap-3">
                     {error && <Error />}
                     <UpdateButton isLoading={isLoading} getMatches={getMatches} />
